577e2c32c4892cc8ef9b16e345f6d4a2a2770a38
Amilissa Araneta <121836249+miss-amilissa@users.noreply.github.com>
2023-11-04 01:37:10 +0800

    feat: day 1 challenge blog card

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,46 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily:{
+        sora: ['Sora', 'sans-serif'],
       },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+      fontSize: {
+        xs: ["0.6875rem", { lineHeight: "1rem" }],
+        sm: ["0.9375rem", { lineHeight: "1.25rem" }],
+        base: ["1rem", { lineHeight: "1.5rem" }],
+        lg: ["1.125rem", { lineHeight: "1.75rem" }],
+        xl: ["1.25rem", { lineHeight: "1.75rem" }],
+        "2xl": ["1.5rem", { lineHeight: "2rem" }],
+        "3xl": ["1.875rem", { lineHeight: "2.25rem" }],
+        "4xl": ["2.25rem", { lineHeight: "2.5rem" }],
+        "5xl": ["3rem", { lineHeight: "1" }],
+        "6xl": ["3.75rem", { lineHeight: "1" }],
+        "7xl": ["4.5rem", { lineHeight: "1" }],
+        "8xl": ["6rem", { lineHeight: "1" }],
+        "9xl": ["8rem", { lineHeight: "1" }],
+      },
+      colors:{
+        "primary": "#883AE1",
+        "secondary": "#E6D6FC",
+        "dark": "#20293A",
+        "dark-2": "#6C727F",
+        "light": "#E5EAF0",
+        "light-2": "#FAFAF9",
+        "white": "#FFFFFF"
+
+      },
+
+      boxShadow: {
+        'bottom-right': '40px 40px 40px rgba(0, 2, 0, 0.2)',
+      },
+
     },
   },
   plugins: [],
